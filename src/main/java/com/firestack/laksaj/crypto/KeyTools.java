a1ed2cee963f3ac8fbfd4f507d94153378828e36
xiaohuo <xiaohuo200@gmail.com>
2018-12-31 20:18:03 +0800

    define account and it's construct, add placeholder in keytools

---
+++ src/main/java/com/firestack/laksaj/crypto/KeyTools.java
@@ -0,0 +1,24 @@
+package com.firestack.laksaj.crypto;
+
+public class KeyTools {
+
+    public static String getAddressFromPrivateKey(String privateKey) {
+        //todo
+        return "";
+    }
+
+    public static String getPublicKeyFromPrivateKey(String privateKey) {
+        //todo
+        return "";
+    }
+
+    public static String getAddressFromPublicKey(String publicKey) {
+        //todo
+        return "";
+    }
+
+    public static boolean VerifyPrivateKey(String privateKey) {
+        //todo
+        return true;
+    }
+}
