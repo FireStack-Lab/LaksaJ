a3052c78ba46fb7ffcf9abb14bd39b7c731f8a5a
xiaohuo <xiaohuo200@gmail.com>
2018-12-31 22:51:36 +0800

    add scrypt deps, scryptwapper

---
+++ src/main/java/com/firestack/laksaj/crypto/ScryptWrapper.java
@@ -0,0 +1,11 @@
+package com.firestack.laksaj.crypto;
+
+import org.spongycastle.crypto.generators.SCrypt;
+
+//http://tools.ietf.org/html/draft-josefsson-scrypt-kdf-01
+public class ScryptWrapper {
+    private static byte[] generateDerivedScryptKey(
+            byte[] password, byte[] salt, int n, int r, int p, int dkLen) {
+        return SCrypt.generate(password, salt, n, r, p, dkLen);
+    }
+}
