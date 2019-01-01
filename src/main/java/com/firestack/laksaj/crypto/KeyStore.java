85cfed70b9c12f7713ffe0280a6ba757ecbb2468
xiaohuo <xiaohuo200@gmail.com>
2019-01-01 17:11:30 +0800

    combine two derivedkey generated into class keystore

--- src/main/java/com/firestack/laksaj/crypto/KeyStore.java
+++ src/main/java/com/firestack/laksaj/crypto/KeyStore.java
@@ -1,4 +1,20 @@
 package com.firestack.laksaj.crypto;
 
+import com.firestack.laksaj.utils.ByteUtil;
+
+import java.io.UnsupportedEncodingException;
+
 public class KeyStore {
+
+    public static byte[] getDerivedKey(byte[] password, KDFParams params) throws UnsupportedEncodingException {
+        if (params instanceof PBKDF2Params) {
+            PBKDF2Params pbkdf2Params = (PBKDF2Params) params;
+            return PBKDF2Wrapper.getDerivedKey(password, ByteUtil.hexStringToByteArray(pbkdf2Params.getSalt()), pbkdf2Params.getCount(), pbkdf2Params.getDkLen());
+        } else if (params instanceof ScryptParams) {
+            ScryptParams scryptParams = (ScryptParams) params;
+            return ScryptWrapper.getDerivedKey(password, ByteUtil.hexStringToByteArray(scryptParams.getSalt()), scryptParams.getN(), scryptParams.getR(), scryptParams.getP(), scryptParams.getDkLen());
+        } else {
+            throw new IllegalArgumentException("unsupport kdf params");
+        }
+    }
 }
