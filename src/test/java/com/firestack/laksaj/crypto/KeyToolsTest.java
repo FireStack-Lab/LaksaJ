8d79b1182df75cd56a045c387caeaa8a6dad0fa6
xiaohuo <xiaohuo200@gmail.com>
2019-01-02 18:19:41 +0800

    complete get address from public key, test it

--- src/test/java/com/firestack/laksaj/crypto/KeyToolsTest.java
+++ src/test/java/com/firestack/laksaj/crypto/KeyToolsTest.java
@@ -14,11 +14,20 @@ public class KeyToolsTest {
     @Test
     public void getPublicKeyFromPrivateKey() {
         String privateKey = "24180e6b0c3021aedb8f5a86f75276ee6fc7ff46e67e98e716728326102e91c9";
-        String publicKey = KeyTools.getPublicKeyFromPrivateKey(privateKey,false);
-        Assert.assertEquals(publicKey.toLowerCase(),"04163fa604c65aebeb7048c5548875c11418d6d106a20a0289d67b59807abdd299d4cf0efcf07e96e576732dae122b9a8ac142214a6bc133b77aa5b79ba46b3e20");
+        String publicKey = KeyTools.getPublicKeyFromPrivateKey(privateKey, false);
+        Assert.assertEquals(publicKey.toLowerCase(), "04163fa604c65aebeb7048c5548875c11418d6d106a20a0289d67b59807abdd299d4cf0efcf07e96e576732dae122b9a8ac142214a6bc133b77aa5b79ba46b3e20");
         privateKey = "b776d8f068d11b3c3f5b94db0fb30efea05b73ddb9af1bbd5da8182d94245f0b";
-        publicKey = KeyTools.getPublicKeyFromPrivateKey(privateKey,false);
-        Assert.assertEquals(publicKey.toLowerCase(),"04cfa555bb63231d167f643f1a23ba66e6ca1458d416ddb9941e95b5fd28df0ac513075403c996efbbc15d187868857e31cf7be4d109b4f8cb3fd40499839f150a");
+        publicKey = KeyTools.getPublicKeyFromPrivateKey(privateKey, false);
+        Assert.assertEquals(publicKey.toLowerCase(), "04cfa555bb63231d167f643f1a23ba66e6ca1458d416ddb9941e95b5fd28df0ac513075403c996efbbc15d187868857e31cf7be4d109b4f8cb3fd40499839f150a");
+    }
 
+    @Test
+    public void getAddressFromPublicKey() {
+        String publicKey = "04cfa555bb63231d167f643f1a23ba66e6ca1458d416ddb9941e95b5fd28df0ac513075403c996efbbc15d187868857e31cf7be4d109b4f8cb3fd40499839f150a";
+        String address = KeyTools.getAddressFromPublicKey(publicKey);
+        Assert.assertEquals(address.toLowerCase(), "0e3e927f8be54eb20f4f47baa2f4d23649433591ea1b967127b971de2288609e");
+        publicKey = "04163fa604c65aebeb7048c5548875c11418d6d106a20a0289d67b59807abdd299d4cf0efcf07e96e576732dae122b9a8ac142214a6bc133b77aa5b79ba46b3e20";
+        address = KeyTools.getAddressFromPublicKey(publicKey);
+        Assert.assertEquals(address.toLowerCase(), "f5b2dfddfdbd0d13a67085a5ad5744c0c6246d9704592116fccbf41978fe99c8");
     }
 }
