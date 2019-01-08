a8675c97ec546deb1f6f7e8ff5e3980ca6ce27b9
xiaohuo <xiaohuo200@gmail.com>
2019-01-08 11:27:35 +0800

    update ds block header struct

--- src/main/java/com/firestack/laksaj/blockchain/DsBlockHeader.java
+++ src/main/java/com/firestack/laksaj/blockchain/DsBlockHeader.java
@@ -1,17 +1,26 @@
 package com.firestack.laksaj.blockchain;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.experimental.Builder;
 
 @Data
 @Builder
 public class DsBlockHeader {
+    @SerializedName("BlockNum")
     private String blockNumber;
+    @SerializedName("Difficulty")
     private int difficulty;
+    @SerializedName("DifficultyDS")
     private int diffcultyDS;
+    @SerializedName("GasPrice")
     private int gasPrice;
+    @SerializedName("LeaderPubKey")
     private String leaderPublicKey;
+    @SerializedName("PoWWinners")
     private String[] powWinners;
+    @SerializedName("PrevHash")
     private String previousHash;
+    @SerializedName("Timestamp")
     private String timestamp;
 }
