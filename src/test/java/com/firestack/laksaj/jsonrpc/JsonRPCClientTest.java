c35f356e6ab49491f1e39c1335b8470b149679b8
xiaohuo <xiaohuo200@gmail.com>
2019-01-08 11:30:32 +0800

    test getdsblock

---
+++ src/test/java/com/firestack/laksaj/jsonrpc/JsonRPCClientTest.java
@@ -0,0 +1,19 @@
+package com.firestack.laksaj.jsonrpc;
+
+import com.firestack.laksaj.blockchain.DsBlock;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+
+public class JsonRPCClientTest {
+
+    @Test
+    public void getDsBlock() throws IOException {
+        JsonRPCClient client = new JsonRPCClient("https://api.zilliqa.com/");
+        DsBlock dsBlock = client.getDsBlock();
+        System.out.println(dsBlock);
+        Assert.assertNotNull(dsBlock);
+    }
+}
