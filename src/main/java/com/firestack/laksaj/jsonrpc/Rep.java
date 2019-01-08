745a4893c5f048b05377c636f1cb755200478207
xiaohuo <xiaohuo200@gmail.com>
2019-01-08 11:29:50 +0800

    define request and response struct, and complete getDsBlock logic

---
+++ src/main/java/com/firestack/laksaj/jsonrpc/Rep.java
@@ -0,0 +1,11 @@
+package com.firestack.laksaj.jsonrpc;
+
+import lombok.Data;
+
+@Data
+public class Rep<R> {
+    private String id;
+    private String jsonrpc;
+    private R result;
+
+}
---
+++ src/main/java/com/firestack/laksaj/jsonrpc/Req.java
@@ -0,0 +1,13 @@
+package com.firestack.laksaj.jsonrpc;
+
+import lombok.Data;
+import lombok.experimental.Builder;
+
+@Data
+@Builder
+public class Req {
+    private String id;
+    private String jsonrpc;
+    private String method;
+    private String[] params;
+}
