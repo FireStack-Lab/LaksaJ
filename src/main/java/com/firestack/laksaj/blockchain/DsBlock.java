f98129fb248f02bbc2ddc0f80eb79320cda3eeab
xiaohuo <xiaohuo200@gmail.com>
2019-01-08 11:30:32 +0800

    test getdsblock

--- src/main/java/com/firestack/laksaj/blockchain/DsBlock.java
+++ src/main/java/com/firestack/laksaj/blockchain/DsBlock.java
@@ -1,10 +1,14 @@
 package com.firestack.laksaj.blockchain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Builder;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DsBlock {
     private DsBlockHeader header;
     private String signature;
