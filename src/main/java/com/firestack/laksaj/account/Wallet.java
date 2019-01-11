393ea8d457a799ae60b8b13b840b44919dee0102
xiaohuo <xiaohuo200@gmail.com>
2019-01-11 18:53:27 +0800

    define Wallet struct, and create account function

--- src/main/java/com/firestack/laksaj/account/Wallet.java
+++ src/main/java/com/firestack/laksaj/account/Wallet.java
@@ -1,4 +1,35 @@
 package com.firestack.laksaj.account;
 
+import com.firestack.laksaj.crypto.KeyTools;
+import com.firestack.laksaj.jsonrpc.Provider;
+
+import java.util.List;
+import java.util.Optional;
+
 public class Wallet {
+    private List<Account> accounts;
+    private Provider provider;
+    private Optional<Account> defaultAccount;
+
+    public Wallet(List<Account> accounts, Provider provider) {
+        this.accounts = accounts;
+        this.provider = provider;
+
+        if (accounts.size() > 0) {
+            defaultAccount = Optional.of(accounts.get(0));
+        } else {
+            Optional.empty();
+        }
+    }
+
+    public String createAccount() {
+        String privateString = KeyTools.generatePrivateKey();
+        Account account = new Account(privateString);
+        this.accounts.add(account);
+
+        if (!defaultAccount.isPresent()) {
+            defaultAccount = Optional.of(accounts.get(0));
+        }
+        return account.getAddress();
+    }
 }
