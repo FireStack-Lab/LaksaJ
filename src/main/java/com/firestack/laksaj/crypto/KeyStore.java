8e62ecb36d7df3c22856117ab4244bfc0ed26817
xiaohuo <xiaohuo200@gmail.com>
2019-01-11 23:27:15 +0800

    refactor some

--- src/main/java/com/firestack/laksaj/crypto/KeyStore.java
+++ src/main/java/com/firestack/laksaj/crypto/KeyStore.java
@@ -5,14 +5,21 @@ import com.firestack.laksaj.utils.ByteUtil;
 import java.io.UnsupportedEncodingException;
 
 public class KeyStore {
+    private PBKDF2Wrapper pbkdf2Wrapper;
+    private ScryptWrapper scryptWrapper;
 
-    public static byte[] getDerivedKey(byte[] password, KDFParams params) throws UnsupportedEncodingException {
+    public KeyStore(PBKDF2Wrapper pbkdf2Wrapper, ScryptWrapper scryptWrapper) {
+        this.pbkdf2Wrapper = pbkdf2Wrapper;
+        this.scryptWrapper = scryptWrapper;
+    }
+
+    public byte[] getDerivedKey(byte[] password, KDFParams params) throws UnsupportedEncodingException {
         if (params instanceof PBKDF2Params) {
             PBKDF2Params pbkdf2Params = (PBKDF2Params) params;
-            return PBKDF2Wrapper.getDerivedKey(password, ByteUtil.hexStringToByteArray(pbkdf2Params.getSalt()), pbkdf2Params.getCount(), pbkdf2Params.getDkLen());
+            return pbkdf2Wrapper.getDerivedKey(password, ByteUtil.hexStringToByteArray(pbkdf2Params.getSalt()), pbkdf2Params.getCount(), pbkdf2Params.getDkLen());
         } else if (params instanceof ScryptParams) {
             ScryptParams scryptParams = (ScryptParams) params;
-            return ScryptWrapper.getDerivedKey(password, ByteUtil.hexStringToByteArray(scryptParams.getSalt()), scryptParams.getN(), scryptParams.getR(), scryptParams.getP(), scryptParams.getDkLen());
+            return scryptWrapper.getDerivedKey(password, ByteUtil.hexStringToByteArray(scryptParams.getSalt()), scryptParams.getN(), scryptParams.getR(), scryptParams.getP(), scryptParams.getDkLen());
         } else {
             throw new IllegalArgumentException("unsupport kdf params");
         }
--- src/test/java/com/firestack/laksaj/crypto/PBKDF2WrapperTest.java
+++ src/test/java/com/firestack/laksaj/crypto/PBKDF2WrapperTest.java
@@ -10,13 +10,14 @@ import java.io.UnsupportedEncodingException;
 public class PBKDF2WrapperTest {
     @Test
     public void generateDerivedScryptKey() throws UnsupportedEncodingException {
+        PBKDF2Wrapper pbkdf2Wrapper = new PBKDF2Wrapper();
         PBKDF2Params params = PBKDF2Params
                 .builder()
                 .salt("0f2274f6c0daf36d5822d97985be5a3d881d11e2e741bad4e038a099eecc3b6d")
                 .count(262144)
                 .dkLen(32)
                 .build();
-        byte[] bytes = PBKDF2Wrapper.getDerivedKey("stronk_password".getBytes(),
+        byte[] bytes = pbkdf2Wrapper.getDerivedKey("stronk_password".getBytes(),
                 ByteUtil.hexStringToByteArray(params.getSalt()), params.getCount(), params.getDkLen());
         byte[] macArray = HashUtil.generateMac(bytes, ByteUtil.hexStringToByteArray("dc55047d51f795509ffb6969db837a4481887ccfb6bfb7c259fb77b19078c2a4"));
         System.out.println(ByteUtil.byteArrayToHexString(macArray));
