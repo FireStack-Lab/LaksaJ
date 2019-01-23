b7a8adc6c179115213e0c31b9357b5f465a5d228
xiaohuo <xiaohuo200@gmail.com>
2019-01-23 12:45:19 +0800

    update message and relative code

--- src/main/java/com/firestack/laksaj/utils/TransactionUtil.java
+++ src/main/java/com/firestack/laksaj/utils/TransactionUtil.java
@@ -13,17 +13,24 @@ public class TransactionUtil {
     public byte[] encodeTransactionProto(TxParams txParams) {
         BigInteger amount = new BigInteger(txParams.getAmount());
         BigInteger gasPrice = new BigInteger(txParams.getGasPrice());
-        return Message.ProtoTransactionCoreInfo.newBuilder()
+        Message.ProtoTransactionCoreInfo.Builder builder = Message.ProtoTransactionCoreInfo.newBuilder()
                 .setVersion(Integer.valueOf(txParams.getVersion()))
                 .setNonce(Strings.isNullOrEmpty(txParams.getNonce()) ? 0 : Long.valueOf(txParams.getNonce()))
                 .setToaddr(ByteString.copyFrom(ByteUtil.hexStringToByteArray(txParams.getToAddr().toLowerCase())))
                 .setSenderpubkey(Message.ByteArray.newBuilder().setData(ByteString.copyFrom(ByteUtil.hexStringToByteArray(txParams.getSenderPubKey()))).build())
                 .setAmount(Message.ByteArray.newBuilder().setData(ByteString.copyFrom(BigIntegers.asUnsignedByteArray(16, amount))).build())
                 .setGasprice(Message.ByteArray.newBuilder().setData(ByteString.copyFrom(BigIntegers.asUnsignedByteArray(16, gasPrice))).build())
-                .setGaslimit(Long.valueOf(txParams.getGasLimit()))
-                .setCode(ByteString.copyFrom(txParams.getCode().getBytes()))
-                .setData(ByteString.copyFrom(txParams.getData().getBytes()))
-                .build().toByteArray();
+                .setGaslimit(Long.valueOf(txParams.getGasLimit()));
+        if (null != txParams.getCode() && !txParams.getCode().isEmpty()){
+            builder.setCode(ByteString.copyFrom(txParams.getCode().getBytes()));
+        }
+
+        if (null != txParams.getData() && !txParams.getData().isEmpty()){
+            builder.setCode(ByteString.copyFrom(txParams.getCode().getBytes()));
+        }
+
+        return builder.build().toByteArray();
+
     }
 
 }
