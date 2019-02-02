4a171ff3d52a15b3ef3c69c8b5076c40c621b6ca
xiaohuo <xiaohuo200@gmail.com>
2019-02-02 18:03:51 +0800

    update doc

--- example/src/main/java/com/firestack/example/App.java
+++ example/src/main/java/com/firestack/example/App.java
@@ -1,5 +1,6 @@
 package com.firestack.example;
 
+import com.firestack.laksaj.blockchain.DsBlock;
 import com.firestack.laksaj.jsonrpc.HttpProvider;
 
 import java.io.IOException;
@@ -7,7 +8,5 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
         HttpProvider client = new HttpProvider("https://api.zilliqa.com/");
-        String networkId = client.getNetworkId();
-        System.out.println(networkId);
     }
 }
