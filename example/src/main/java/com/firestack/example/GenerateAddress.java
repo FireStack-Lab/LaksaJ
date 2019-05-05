aae9d47e7d30712633cc34f1ede24282c5c94938
xiaohuo <xiaohuo200@gmail.com>
2019-05-06 00:25:57 +0800

    chore: update version info

--- example/src/main/java/com/firestack/example/GenerateAddress.java
+++ example/src/main/java/com/firestack/example/GenerateAddress.java
@@ -2,11 +2,13 @@ package com.firestack.example;
 
 import com.firestack.laksaj.crypto.KeyTools;
 import com.firestack.laksaj.utils.ByteUtil;
+import org.web3j.crypto.ECKeyPair;
 
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+
 
 public class GenerateAddress {
     //How to generate large amount of addresses
