85f9d23894b4cd5b00c0ca92b9a7dfc61bd44bc5
xiaohuo <xiaohuo200@gmail.com>
2021-01-02 21:40:22 +0800

    chore: update example project

--- example/src/main/java/com/firestack/example/SmartContractDeposit.java
+++ example/src/main/java/com/firestack/example/SmartContractDeposit.java
@@ -1,17 +1,14 @@
 package com.firestack.example;
 
-import com.firestack.laksaj.blockchain.Contract;
 import com.firestack.laksaj.exception.ZilliqaAPIException;
 import com.firestack.laksaj.jsonrpc.HttpProvider;
 
 import java.io.IOException;
-import java.util.List;
-
 public class SmartContractDeposit {
     public static void main(String[] args) throws IOException, InterruptedException, ZilliqaAPIException {
         HttpProvider client = new HttpProvider("https://api.zilliqa.com/");
         String lastEpoch = client.getNumTxnsTxEpoch().getResult();
-        List<Contract.State> lastStateList = client.getSmartContractState("D6606D02DFF929593312D8D0D36105E376F95AA0").getResult();
+        String lastStateList = client.getSmartContractState("D6606D02DFF929593312D8D0D36105E376F95AA0");
 
         System.out.println("last epoch is " + lastEpoch);
         System.out.println("last state:" + lastStateList);
@@ -23,7 +20,7 @@ public class SmartContractDeposit {
             System.out.println(n + "th current epoch is: " + epoch);
             if (!lastEpoch.equals(epoch)) {
                 System.out.println("epoch hash changed");
-                List<Contract.State> stateList = client.getSmartContractState("D6606D02DFF929593312D8D0D36105E376F95AA0").getResult();
+                String stateList = client.getSmartContractState("D6606D02DFF929593312D8D0D36105E376F95AA0");
                 System.out.println("last state: " + lastStateList);
                 System.out.println("current state: " + stateList);
                 lastEpoch = epoch;
